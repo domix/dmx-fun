ac67250420a4480bd3123dca4de5b8f71d33fdec
Domingo Suarez Torres <domingo.suarez@gmail.com>
2025-11-12 16:39:47 -0600

    Adds some docs

--- lib/src/test/java/codes/domix/fun/example/UserService.java
+++ lib/src/test/java/codes/domix/fun/example/UserService.java
@@ -1,56 +1,63 @@
 package codes.domix.fun.example;
 
 import codes.domix.fun.Result;
+import java.util.regex.Pattern;
+
 
 public class UserService {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+        "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$",
+        Pattern.CASE_INSENSITIVE
+    );
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{10,30}$"
+    );
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    protected Result<CreateUserCommand, String> isValidEmail(CreateUserCommand command) {
-        if (command.email().contains("@")) {
-            return Result.ok(command);
+    protected Result<CreateUserCommand, String> isValidEmail(
+        CreateUserCommand command
+    ) {
+        var email = command.email();
+        if (email == null || email.isBlank()) {
+            return Result.err("Provided email is either null or blank");
         }
-        return Result.err("Invalid email");
+        boolean emailMatches = EMAIL_PATTERN
+            .matcher(email)
+            .matches();
+
+        if (!emailMatches) {
+            return Result.err("Invalid email");
+        }
+
+        return Result.ok(command);
     }
 
-    protected Result<CreateUserCommand, String> isValidPassword(CreateUserCommand command) {
+    protected Result<CreateUserCommand, String> isValidPassword(
+        CreateUserCommand command
+    ) {
 
-        if (command.password().length() >= 6) {
-            return Result.ok(command);
+        var password = command.password();
+        if (password == null) {
+            return Result.err("Provided password is null");
+        }
+        boolean validPassword = PASSWORD_PATTERN
+            .matcher(password)
+            .matches();
+
+        if (!validPassword) {
+            return Result.err("Invalid password.");
         }
 
-        return Result.err("Invalid password");
+        return Result.ok(command);
     }
 
     public Result<User, String> createUser(CreateUserCommand command) {
-        Result<Result<CreateUserCommand, String>, String> map = this.isValidEmail(command)
-            .map(this::isValidPassword);
-
-        Result<CreateUserCommand, String> validPassword = this.isValidPassword(command);
-
-        if (validPassword.isOk()) {
-            CreateUserCommand createUserCommand = validPassword.get();
-        } else {
-
-        }
-
-        final var result = this.isValidEmail(command)
+        return this.isValidEmail(command)
             .flatMap(this::isValidPassword)
             .flatMap(this.userRepository::createUser);
-
-        // 1. Validar email
-        if (this.isValidEmail(command).isError()) {
-            throw new IllegalArgumentException("Invalid email");
-        }
-        // 2. Validar password
-        if (this.isValidPassword(command).isError()) {
-            throw new IllegalArgumentException("Invalid password");
-        }
-
-        // 3. Guardar en la Base de datos
-        return this.userRepository.createUser(command);
     }
 }
