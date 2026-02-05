ea188ef320ff78cba501f65462216c1fdc7d6d20
Domingo Suarez Torres <domingo.suarez@gmail.com>
2026-02-05 10:44:10 -0600

    minor fix

--- site/src/config/site.ts
+++ site/src/config/site.ts
@@ -1,6 +1,6 @@
 export const site = {
     slug: "dmx-fun",
-    title: "dmx-fun",
+    author: "Domingo Suarez Torres",
     description: "A functional programming library for Java",
     tagline: "Pragmatic FP utilities for Java",
     github: "https://github.com/domix/dmx-fun",
