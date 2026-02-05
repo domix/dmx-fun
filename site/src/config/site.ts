0bb539fd37197fd2f1df4d55966ab2cf4698bd0f
Domingo Suarez Torres <domingo.suarez@gmail.com>
2026-02-04 18:44:23 -0600

    initial website implementation

---
+++ site/src/config/site.ts
@@ -0,0 +1,8 @@
+export const site = {
+    slug: "dmx-fun",
+    title: "dmx-fun",
+    description: "A functional programming library for Java",
+    tagline: "Pragmatic FP utilities for Java",
+    github: "https://github.com/domix/dmx-fun",
+} as const;
+export type Site = typeof site;
